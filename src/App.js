410f0313d9c167b8c2eeba725003468353bdbffd
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-02-26 15:41:31 -0800

    api works and outputs on console

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 export default function App() {
   // this is a funtion called getAdvice that calls an api to return the advice
   async function getAdvice() {
-    const res = await fetch("https://api.adiceslip.com/advice");
+    const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    console.log(data);
+    console.log(data.slip.advice);
   }
   return (
     <div>
       <h1>Hello World!</h1>
-      <button>Get Some Killer Advice</button>
+      <button onClick={getAdvice}>Get Some Killer Advice</button>
     </div>
   );
 }
