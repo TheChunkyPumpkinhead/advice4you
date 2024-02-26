3d9151f39776cdfadf56f60dbc8cd0301521c61e
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-02-26 15:30:55 -0800

    added api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 export default function App() {
+  // this is a funtion called getAdvice that calls an api to return the advice
+  async function getAdvice() {
+    const res = await fetch("https://api.adiceslip.com/advice");
+    const data = await res.json();
+    console.log(data);
+  }
   return (
     <div>
       <h1>Hello World!</h1>
