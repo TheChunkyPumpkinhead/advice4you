6cc7c749412104279fcbf382920b30ec2f971edf
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-02-27 12:55:16 -0800

    added api and a function to display api called

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+// this is the function for diplaying the advice from getAdvice funtion
 export default function App() {
-  // this is a funtion called getAdvice that calls an api to return the advice
+  const [advice, setAdvice] = useState("");
+
+  // this is a function called getAdvice that calls an api to return the advice
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
   }
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>{advice}</h1>
+      {/* this function is called when clicked */}
       <button onClick={getAdvice}>Get Some Killer Advice</button>
     </div>
   );
